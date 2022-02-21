a36074fb9f472471a5bd8cc3121785d130678398
Nordo80 <36612428+Nordo80@users.noreply.github.com>
2022-02-21 16:37:18 +0200

    ex04fix

--- src/oo/hide/EqualityTests.java
+++ src/oo/hide/EqualityTests.java
@@ -5,13 +5,11 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotSame;
 
 public class EqualityTests {
     @Test
     public void test(){
-        Point a = new Point(2, 2);
         new Point(1, 2).equals(new Point(1, 2));
     }
 
